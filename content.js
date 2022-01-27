34603fab43b1f3c0ab79402ecc55b253301e983e
Gregory Huczynski <gh_online@me.com>
2022-01-27 14:19:21 +0000

    Round 2 on extension demo

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ var times = 20;
 for(var i = 0; i < times; i++){
     var img = new Image();
     img.setAttribute('style', 'display: block;')
-    img.setAttribute('src', 'https://picsum.photos/id/237/200/300');
+    img.setAttribute('src', 'dog.jpeg');
     img.setAttribute('loading', ((i == 0) ? 'auto' : 'lazy'));
     document.body.appendChild(img);
 }
